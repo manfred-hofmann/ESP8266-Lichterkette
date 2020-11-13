6c852712a6b1c4b012496326ee3890e29dc8f3c7
manfred-hofmann <hofmann-forum01@online.de>
2020-11-13 17:37:37 +0100

    neu

--- Colors.h
+++ Colors.h
@@ -5,6 +5,8 @@ Colors.h
 #ifndef COLORS_H
 #define COLORS_H
 
+#include "Configuration.h"
+
 struct color_s
 {
 	uint8_t red;
@@ -12,8 +14,15 @@ struct color_s
 	uint8_t blue;
 };
 
-String s_color[9] = { "#ff0000","#ff8000","#ffff00","#00ff00", "#0080ff", "#0000ff", "#8000ff", "#ff0080"};
+//String s_color[10] = { "#ff0000","#ff8000","#ffff00","#00ff00", "#0080ff", "#0000ff", "#8000ff", "#ff0080"};
 
-color_s w_color[9];
+color_s w_color[10];
+//color_s mysetting_w_color[NUM_SETTINGS][9];
 
+// farben
+#define GRUEN 0x007f00
+#define WEISS 0x7f7f7f
+#define ROT   0x7f0000
+#define BLAU  0x00007f
+#define LILA  0x7f007f
 #endif
